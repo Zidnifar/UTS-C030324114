2c151336199745219555f720240b193e707da103
Zidnifar <zidnifarahmazida@gmail.com>
2025-05-26 20:52:34 +0800

    Mengubah tampilan menu

--- menu.cpp
+++ menu.cpp
@@ -18,15 +18,6 @@ void Menu(){
 }
 
 void inputData(){
-    system("cls");
-    cout << "Masukkan jumlah elemen array (max "<< MAX_SIZE <<"): ";
-    cin >> n;
-
-    if (n <= 0 || n > MAX_SIZE){
-        cout << "Jumlah elemen tidak valid!\n";
-        cin.get();
-        return;
-    }
     cout << "Masukkan" << n << "elemen array: " << endl;
     for (int i = 0; i < n; i++){
         cout << "Elemen ke-" << (i+1) << ":";
