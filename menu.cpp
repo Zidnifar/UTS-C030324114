90e96c010690803972972d3e34fe8d85b741baf9
Zidnifar <zidnifarahmazida@gmail.com>
2025-05-26 20:38:51 +0800

    Mengubah tampilan menu

--- menu.cpp
+++ menu.cpp
@@ -7,15 +7,14 @@ int arr[MAX_SIZE];
 int n = 0; 
 
 void Menu(){
-    system("cls");
-    cout << "Aplikasi Sorting Bubble" << "\n";       
-    cout << "1. Memasukkan data" << "\n";            
-    cout << "2. Menampilkan data" << "\n";            
-    cout << "3. Sorting" << "\n";           
-    cout << "4. Sepatah kata" << "\n";            
-    cout << "5. Exit" << "\n";           
-    cout << "Masukkan angka :";        
-}
+    cout << "Aplikasi sorting bubble" << "\n";       
+    cout << "1. Memasukkan Data" << "\n";            
+    cout << "2. Menampilkan Data" << "\n";            
+    cout << "3. sorting" << "\n";           
+    cout << "4. sepatah kata" << "\n";            
+    cout << "5. exit" << "\n";           
+    cout << "Masukan angka :";        
+ }
 
 void inputData(){
     system("cls");
